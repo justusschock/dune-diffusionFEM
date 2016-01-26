11954304f7792e25608537a0d3d470337a35f4bf
Justus Schock <justus.schock@rwth-aachen.de>
2016-01-26 22:57:45 +0100

    fixed MPI -initialization

--- dune-diffusionFEM/src/dune-diffusionFEM.cc
+++ dune-diffusionFEM/src/dune-diffusionFEM.cc
@@ -1,25 +1,25 @@
 #ifdef HAVE_CONFIG_H
-# include "config.h"
 #endif
 #include <iostream>
 #include <dune/common/parallel/mpihelper.hh> // An initializer of MPI
-#include <dune/common/exceptions.hh> // We use exceptions
+#include <dune/fem/misc/mpimanager.hh>
 
 int main(int argc, char** argv)
 {
-  try{
-    // Maybe initialize MPI
-    Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
+    try{
+        // Maybe initialize MPI
+        Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
+        Dune::Fem::MPIManager::initialize(argc, argv);
 
-    return 0;
-  }
-  catch (Dune::Exception &e){
-    std::cerr << "Dune reported error: " << e << std::endl;
-  }
-  catch (std::exception &e){
-    std::cerr << "STL reported error: " << e.what() << std::endl;
-  }
-  catch (...){
-    std::cerr << "Unknown exception thrown!" << std::endl;
-  }
+        return 0;
+    }
+    catch (Dune::Exception &e){
+        std::cerr << "Dune reported error: " << e << std::endl;
+    }
+    catch (std::exception &e){
+        std::cerr << "STL reported error: " << e.what() << std::endl;
+    }
+    catch (...){
+        std::cerr << "Unknown exception thrown!" << std::endl;
+    }
 }
