f41e5b3b1aeb8eb2fb2371241e18d7af1213d1c8
Justus Schock <justus.schock@rwth-aachen.de>
2016-01-26 21:33:06 +0100

    deleted unnecessary stuff in dune-diffusionFEM

--- dune-diffusionFEM/src/dune-diffusionFEM.cc
+++ dune-diffusionFEM/src/dune-diffusionFEM.cc
@@ -10,12 +10,7 @@ int main(int argc, char** argv)
   try{
     // Maybe initialize MPI
     Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
-    std::cout << "Hello World! This is dune-diffusionFEM." << std::endl;
-    if(Dune::MPIHelper::isFake)
-      std::cout<< "This is a sequential program." << std::endl;
-    else
-      std::cout<<"I am rank "<<helper.rank()<<" of "<<helper.size()
-        <<" processes!"<<std::endl;
+
     return 0;
   }
   catch (Dune::Exception &e){
