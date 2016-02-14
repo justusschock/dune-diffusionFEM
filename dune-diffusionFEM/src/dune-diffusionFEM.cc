3783e02801b9459ca3ff7355040ec8f758dcb7e9
Justus Schock <justus.schock@rwth-aachen.de>
2016-02-14 17:15:16 +0100

    elliptic Operator

--- dune-diffusionFEM/src/dune-diffusionFEM.cc
+++ dune-diffusionFEM/src/dune-diffusionFEM.cc
@@ -2,16 +2,28 @@
 #endif
 #include <iostream>
 #include <dune/common/parallel/mpihelper.hh> // An initializer of MPI
-#include <dune/fem/misc/mpimanager.hh>
+#include <dune/grid/yaspgrid.hh>
+
+#include "diffusionPDE.hh"
 
 int main(int argc, char** argv)
 {
     try{
         // Maybe initialize MPI
         Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
-        Dune::Fem::MPIManager::initialize(argc, argv);
+
+        const int dim = 2;
+        Dune::FieldVector<double, dim> L(1.0);
+        Dune::array<int, dim> N (Dune::fill_array<int, dim>(1));
+        std::bitset<dim> B(false);
+        Dune::YaspGrid<dim> grid(L,N,B,false);
+
 
         return 0;
+    }
+
+    catch (std::string &e){
+        std::cerr << e << std::endl;
     }
     catch (Dune::Exception &e){
         std::cerr << "Dune reported error: " << e << std::endl;
@@ -23,3 +35,4 @@ int main(int argc, char** argv)
         std::cerr << "Unknown exception thrown!" << std::endl;
     }
 }
+
